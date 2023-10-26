36a23ccb535623f31acffa08c7ce768d9a97e10c
BalazsBanfi <banfibalazs81@gmail.com>
2023-10-26 18:03:27 +0200

    Add possibleMoves

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-
 // Create the chessboard
 const createBoard = () => {
   return new Array(8).fill(new Array(8).fill(null));
 };
 
+const possibleMoves = [
+  [2, 1],
+  [2, -1],
+  [1, 2],
+  [1, -2],
+  [-2, 1],
+  [-2, -1],
+  [-1, 2],
+  [-1, -2],
+];
+
 const knightMoves = ([startX, startY], [endX, endY]) => {
   console.log(startX, startY, endX, endY);
-  
-
 };
 
 const board = createBoard();
